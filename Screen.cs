4db43eb3f31576d49b89cba5321e55c2ce5ba56a
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-03-22 11:16:17 -0300

    :sparkles: feat: add new class 'Screen' for generalized attributes and methods

---
+++ Screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CartagenaBuenaventura
+{
+    public class Screen : Form
+    {
+        public Screen() 
+        {
+            this.Dock = DockStyle.Fill;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.TopLevel = false;
+            //this.Show();
+        }
+    }
+}
