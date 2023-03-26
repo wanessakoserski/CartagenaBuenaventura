1ebeece6abe953dcb1f16c0e8e69a69d994b922b
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-03-26 01:03:54 -0300

    :sparkles: feat: add implementation of class 'Move'

---
+++ Classes/Move.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartagenaBuenaventura.Classes
+{
+    internal class Move
+    {
+        public uint id;
+        public Player player;
+        public Card card;
+        public uint position;
+    }
+}
