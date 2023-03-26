32d6c891364038df42cccfda97c36814ef6a32dd
Wanessa Koserski <wkoserski@gmail.com>
2023-03-25 22:30:46 -0300

    :sparkles: feat: create 'Game' class

---
+++ Classes/Game.cs
@@ -0,0 +1,24 @@
+﻿using CartagenaServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartagenaBuenaventura.Classes
+{
+    internal class Game
+    {
+        public void ListMatches()
+        {
+            string[] matches = Jogo.ListarPartidas("T").Split('\n');
+            /*
+            foreach (string match in matches)
+            {
+                if(match != "")
+                    Console.WriteLine("> " + match);
+            }
+            */
+        }
+    }
+}
