24fcc1688147fa842e0bbd09a9e275e53b2295bb
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-03-26 01:01:04 -0300

    :sparkles: feat: add implementation of class 'Player'

---
+++ Classes/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartagenaBuenaventura.Classes
+{
+    internal class Player
+    {
+        public uint id;
+        public string name;
+        public Color color;
+        public string password;
+        public List<Move> moves;
+        public List<Card> hand;
+
+    }
+}
