3d45352c759f52623e8109a1e8db3a9ac8c48445
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-03-26 13:10:11 -0300

    :sparkles: feat: add implementation of CreateMatch function on class Game

--- Classes/Game.cs
+++ Classes/Game.cs
@@ -20,5 +20,20 @@ namespace CartagenaBuenaventura.Classes
             }
             */
         }
+
+        // Receive two strings  name and password as parameters, being name length < 20 and
+        // name != from any match name in the server, and password length <= 10. Create a
+        // new match and return it
+        public Match CreateMatch(string name, string password) 
+        {
+            Match match = new Match();
+            match.name = name;
+            match.password = password;
+            match.status = enums.MatchStatus.Open;
+
+            match.id = Convert.ToUInt32(Jogo.CriarPartida(name, password));
+
+            return match;
+        }
     }
 }
