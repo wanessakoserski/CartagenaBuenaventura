3c7a1bd763d7264044040a22e88cf5c52c6e91d1
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-03-26 00:56:03 -0300

    :sparkles: feat: add implementation of class 'Match'

---
+++ Classes/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartagenaBuenaventura.Classes
+{
+    internal class Match
+    {
+        public uint id;
+        public string name;
+        public DateTime creationDate;
+        public Status status;
+        public string password;
+        public List<Player> players;
+    }
+}
