78d10979368b302a147a20eb48921655a03c7578
Wanessa Koserski <wkoserski@gmail.com>
2023-03-26 21:45:45 -0300

    :sparkles: feat: create 'Tile' class

---
+++ Classes/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartagenaBuenaventura.Classes
+{
+    internal class Tile
+    {
+    }
+}
