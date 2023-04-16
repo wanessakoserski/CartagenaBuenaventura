a796392aee2210c33b8bdee7cb6246ba3aaf48b6
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-04-16 05:08:09 -0300

    :sparkles: feat: add implementation of 'Move()' method of class 'Pawn'

--- Classes/Pawn.cs
+++ Classes/Pawn.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,11 +16,14 @@ namespace CartagenaBuenaventura.Classes
         public Player player;
         public Color color;
         public PictureBox img;
+        public uint position;
+        private int spotIndex;
 
         public Pawn(Player player)
         {
             this.player = player;
             this.color = (Color)player.color;
+            this.position = 0;
 
             img = new PictureBox();
             img.BackColor = System.Drawing.Color.Transparent;
@@ -37,6 +41,43 @@ namespace CartagenaBuenaventura.Classes
             else if (this.color == Color.Yellow) { img.Image = Properties.Resources.yellow; }
             else if (this.color == Color.Blue) { img.Image = Properties.Resources.blue; }
             else if (this.color == Color.Brown) { img.Image = Properties.Resources.brown; }
+        }
+
+        // Move a pawn PictureBox to the desired location on the Board, meaning that the pawn will be
+        // placed in the available position inside the destination tile
+        public void Move(Move move, List<Tile> board) 
+        {
+            this.position = move.destination;
+
+            Tile origin = board[Convert.ToInt32(move.origin)];
+            Tile destination = board[Convert.ToInt32(move.destination)];
+
+            origin.spotAvailable[this.spotIndex] = true;
+
+            if (destination.spotAvailable[0])
+            {
+                img.Location = destination.location;
+                destination.spotAvailable[0] = false;
+                this.spotIndex = 0;
+            }
+            else if (destination.spotAvailable[1])
+            {
+                Point aux = destination.location;
+                aux.X += 15;
+                img.Location = aux;
+
+                destination.spotAvailable[1] = false;
+                this.spotIndex = 1;
+            }
+            else 
+            {
+                Point auxLocation = destination.location;
+                auxLocation.Y += 15;
+                img.Location = auxLocation;
+
+                destination.spotAvailable[2] = false;
+                this.spotIndex = 2;
+            }
         }
     }
 }
