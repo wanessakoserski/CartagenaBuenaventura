e44b58b33b1487de9f02da1194b398c85154b4d5
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-04-16 05:09:22 -0300

    :sparkles: feat: add properties 'location' and 'spotAvailable' to 'Tile' class

--- Classes/Tile.cs
+++ Classes/Tile.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CartagenaBuenaventura.Classes
 {
@@ -10,5 +12,7 @@ namespace CartagenaBuenaventura.Classes
     {
         public string symbol;
         public int position;
+        public Point location;
+        public bool[] spotAvailable = new[] { true, true, true };
     }
 }
