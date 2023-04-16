a68f341dfda87de8c57f6cdab16150e5af10329f
Luis-HCarvalho <luishenriquecm12@gmail.com>
2023-04-16 03:56:11 -0300

    :sparkles: feat: include changes to class 'Pawn' constructor, such as, conditional statements to decide wich pawn image is to be used, and assignments of values to the attribute of 'img'(PictureBox) properties

--- Classes/Pawn.cs
+++ Classes/Pawn.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CartagenaBuenaventura.Properties;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +16,27 @@ namespace CartagenaBuenaventura.Classes
         public Color color;
         public PictureBox img;
 
-        public Pawn(Player player, Color color) 
+        public Pawn(Player player)
         {
             this.player = player;
-            this.color = color;
+            this.color = (Color)player.color;
+
+            img = new PictureBox();
+            img.BackColor = System.Drawing.Color.Transparent;
+            img.SizeMode = PictureBoxSizeMode.CenterImage;
+            img.Margin = new Padding(0);
+            img.Size = new Size(15, 15);
+
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddEllipse(0, 0, img.Width - 2, img.Height - 2);
+            Region rg = new Region(gp);
+            img.Region = rg;
+
+            if (this.color == Color.Red) { img.Image = Properties.Resources.red; }
+            else if (this.color == Color.Green) { img.Image = Properties.Resources.green; }
+            else if (this.color == Color.Yellow) { img.Image = Properties.Resources.yellow; }
+            else if (this.color == Color.Blue) { img.Image = Properties.Resources.blue; }
+            else if (this.color == Color.Brown) { img.Image = Properties.Resources.brown; }
         }
     }
 }
